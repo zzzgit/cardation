29261662fce37115d729b208ce4bf82fce44e14c
jonkee <zzzgoo@gmail.com>
2022-02-25 11:36:58 +0800

    build: replace rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 const path = require("path")
 // const babel = require("rollup-plugin-babel")
-const replace = require("rollup-plugin-replace")
-import typescript from "rollup-plugin-typescript2"
 const pkg = require("./package.json")
+const replace = require("@rollup/plugin-replace")
+const typescript = require("@rollup/plugin-typescript")
 
 module.exports = {
 	input: path.resolve(__dirname, "./src/index.ts"),
@@ -22,13 +22,8 @@ module.exports = {
 			NODE_ENV: JSON.stringify(process.env.NODE_ENV === "production" ? "production" : "develop"),
 		}),
 		typescript({
-			cacheRoot: "./.rts2_cache",
-			verbosity: 3,
-			useTsconfigDeclarationDir: true,
-			clean: false,
-			tsconfigOverride: {
-				compilerOptions: {"module": "ESNext"},
-			},
+			cacheDir: "./.rts2_cache",
+			compilerOptions: {"module": "ESNext"},
 		}),
 	],
 
