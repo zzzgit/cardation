28ecc6b49debac6292b99509d7335e40d263542a
jonkee <zzzgoo@gmail.com>
2024-01-13 18:06:45 +0800

    refactor: change rollup config to es module

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
 /* eslint-env node */
-const path = require("path")
-const pkg = require("./package.json")
-const replace = require("@rollup/plugin-replace")
-const typescript = require("@rollup/plugin-typescript")
+import path from 'path'
+import pkg from './package.json'
+import replace from '@rollup/plugin-replace'
+import typescript from '@rollup/plugin-typescript'
 
-module.exports = {
-	input: path.resolve(__dirname, "./src/index.ts"),
+export default {
+	input: path.resolve(__dirname, './src/index.ts'),
 	output: {
 		file: path.resolve(__dirname, pkg.main),
-		format: "commonjs",
-		name: "cardation",
-		banner: "/* eslint-disable */",
+		format: 'commonjs',
+		name: 'cardation',
+		banner: '/* eslint-disable */',
 		// sourcemap: true,
 	},
 	plugins: [
 		replace({
 			NODE_ENV: JSON.stringify(
-				process.env.NODE_ENV === "production" ? "production" : "develop"
+				process.env.NODE_ENV === 'production' ? 'production' : 'develop'
 			),
 		}),
 		typescript({
-			cacheDir: "./.rts2_cache",
-			compilerOptions: {module: "ESNext"},
+			cacheDir: './.rts2_cache',
+			compilerOptions: { module: 'ESNext' },
 		}),
 	],
 }
