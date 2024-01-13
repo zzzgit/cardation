0a51fa3dd601aa8cc54ca0186b493d0d51315fd3
jonkee <zzzgoo@gmail.com>
2024-01-13 22:46:59 +0800

    refactor: modify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,30 @@
-/* eslint-env node */
 import path from 'path'
-import pkg from './package.json'
-import replace from '@rollup/plugin-replace'
+import { fileURLToPath } from 'url'
+import pkg from './package.json' assert { type: 'json' }
 import typescript from '@rollup/plugin-typescript'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default {
 	input: path.resolve(__dirname, './src/index.ts'),
 	output: {
 		file: path.resolve(__dirname, pkg.main),
-		format: 'commonjs',
+		// dir: 'built',
+		format: 'commonjs', // 輸出格式
 		name: 'cardation',
+		chunkFileNames: '[name].aaa.js',
 		banner: '/* eslint-disable */',
 		// sourcemap: true,
 	},
 	plugins: [
-		replace({
-			NODE_ENV: JSON.stringify(
-				process.env.NODE_ENV === 'production' ? 'production' : 'develop'
-			),
-		}),
 		typescript({
 			cacheDir: './.rts2_cache',
-			compilerOptions: { module: 'ESNext' },
+			compilerOptions: {
+				module: 'ESNext', // 輸入格式
+				// outDir: './built/lib/',
+				declarationDir: 'built/t',
+			},
 		}),
 	],
 }
